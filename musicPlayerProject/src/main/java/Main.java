3f13f34bc2212a044842822d366a6f51a9a96fb8
George Christoforidis <georgechristoforidis2003@gmail.com>
2024-11-03 19:25:07 +0200

    new features this way is wrong but it's fine

---
+++ musicPlayerProject/src/main/java/Main.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            MusicPlayerFrame mpf = new MusicPlayerFrame(1080,720);
+        });
+    }
+}
---
+++ musicPlayerProject/src/main/java/SongLoader.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.util.List;
+import java.util.ArrayList;
+
+public class SongLoader {
+    // path from src to the folder of songs
+    public static List<String> loadFromFolder(String path) {
+        List<String> pathsOfSongs = new ArrayList<>();
+        File folder = new File(path);
+        if (folder.exists() && folder.isDirectory()) {
+            File[] songsInsideFolder = folder.listFiles();
+            if (songsInsideFolder == null) {
+                System.out.println("There are not any songs here!");
+            } else {
+                for (File file : songsInsideFolder) {
+                    pathsOfSongs.add(path + file.getName());
+                }
+            }
+        } else {
+            System.out.println("Folder " + path + " does not exist or is not accessible!");
+        }
+        return pathsOfSongs;
+    }
+    public static List<String> loadFromDataBase(String path) {
+        // TODO
+        return null;
+    }
+    public static List<String> loadFromAPI(String path) {
+        // TODO
+        return null;
+    }
+}
