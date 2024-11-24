8179ae99f9d819f07ea734796c4c3f7997621fc9
johngiannis <giannisdem1@protonmail.com>
2024-11-24 23:59:38 +0200

    Authentication Done

--- musicPlayerProject/src/main/java/Main.java
+++ musicPlayerProject/src/main/java/Main.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            MusicPlayerFrame mpf = new MusicPlayerFrame(1080,720);
+            main.java.MusicPlayerFrame mpf = new main.java.MusicPlayerFrame(1080,720);
         });
     }
 }
